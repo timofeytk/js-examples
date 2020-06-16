68343657dcac80df21c2f1d996aa790be0702162
Timur Timofeev <timofeytk@yandex.ru>
2020-06-16 20:10:13 +0600

    index, styles modified

--- index.php
+++ index.php
@@ -10,7 +10,43 @@
 <body>
     <div class="container">
         <div class="quiz">
-            <div class="title">Простой квиз на 7 вопросов</div>
+            <div class="quiz__head">
+                <div class="title">Простой квиз на 7 вопросов</div>
+                <div class="quiz__process">
+                    Пройдено <div class="quiz__percent"></div>
+                </div>
+                <div class="quiz-item__progress">
+                    <div class="quiz__visual">
+                        <div class="quiz-circle quiz-circle-0 quiz-circle-active">
+                        </div>
+                        <div class="quiz-block quiz-block-0">
+                        </div>
+                        <div class="quiz-circle quiz-circle-1 ">
+                        </div>
+                        <div class="quiz-block quiz-block-1">
+                        </div>
+                        <div class="quiz-circle quiz-circle-2">
+                        </div>
+                        <div class="quiz-block quiz-block-2">
+                        </div>
+                        <div class="quiz-circle quiz-circle-3">
+                        </div>
+                        <div class="quiz-block quiz-block-3">
+                        </div>
+                        <div class="quiz-circle quiz-circle-4">
+                        </div>
+                        <div class="quiz-block quiz-block-4">
+                        </div>
+                        <div class="quiz-circle quiz-circle-5">
+                        </div>
+                        <div class="quiz-block quiz-block-5">
+                        </div>
+                        <div class="quiz-circle quiz-circle-6">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </body>
