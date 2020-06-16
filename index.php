e1b9a813646c69d2b2185e5ae035150a6bb6e9aa
Timur Timofeev <timofeytk@yandex.ru>
2020-06-16 08:49:02 +0600

    Initial commit.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Простой квиз</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="quiz">
+            <div class="title">Простой квиз на 7 вопросов</div>
+        </div>
+    </div>
+</body>
+</html>
